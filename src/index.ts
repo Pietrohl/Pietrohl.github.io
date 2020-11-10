49e0a723fa8c4689597367cf0b95f82c65a4e329
Pietro Henrique <pietro_henrique4@hotmail.com>
2020-11-10 01:25:01 -0300

    Firs recorvery of master branch

---
+++ src/index.ts
@@ -0,0 +1,123 @@
+import $ from 'jquery'; 
+import debounce from 'lodash.debounce';
+import throttle from 'lodash.throttle';
+
+//Implementação Lazy Scroll por JQuery
+(function(){
+     $('a')?.click(function(){
+
+        let element = <JQuery<HTMLElement>>$(this);
+        let refString: string = <string>element.attr('href')
+        let documentTop: number =<number>$(<Document>document).scrollTop();
+        let scrollPosition: number = <number>$(refString).offset()?.top;
+        
+
+        $('html, body').animate(
+            {scrollTop: scrollPosition}
+            , Math.abs(scrollPosition-documentTop)+500);
+        return false;
+    })
+}());
+
+
+//Implementação de funções vinculadas ao Scroll
+(function(){
+
+    //Fade-in borda do menu de navegação
+    function navBorderShow() {
+
+        let navbarBorder: HTMLElement = <HTMLElement>document.getElementById("navbar-boder")
+        
+        if(navbarBorder.style.marginTop != '0px') {
+            navbarBorder.style.marginTop = '0px';
+            navbarBorder.animate([ 
+                {marginTop: '-60px', easing: 'ease-out'}, 
+                {marginTop: '0px'}], 
+                {
+                duration: 750,
+                iterations: 1
+            });
+        }
+
+    }
+
+    //Fade-out Borda de Navegação
+    function navBorderHide() {
+
+        let navbarBorder: HTMLElement = <HTMLElement>document.getElementById("navbar-boder")
+        if(navbarBorder.style.marginTop != '-60px') {
+            navbarBorder.style.marginTop = '-60px';
+            navbarBorder.animate([ 
+                {marginTop: '0px', easing: 'ease-out'}, 
+                {marginTop: '-60px'}], 
+                {
+                duration: 1500,
+                iterations: 1
+            });
+        }
+    }
+
+    
+
+    //Fade-out do título Hero Banner
+    function AnimateFadeout() {
+        
+        let documentTop: number = <number>$(<Document>document)?.scrollTop();
+        let opacityLimit: number = window.innerHeight/2;
+        let opacityLevel: number = documentTop<=(opacityLimit) ? ((opacityLimit-documentTop)/opacityLimit) : 0;        
+        let navbarAnimate: HTMLElement = <HTMLElement>document.getElementById("fadeout-animate")
+        
+        if(documentTop==0) {
+            navBorderHide();
+            navbarAnimate.classList.add('animate-pulse');
+            navbarAnimate.setAttribute("style",`opacity: '1'; visibility: visible`);
+            
+        } else {            
+            navbarAnimate.classList.remove('animate-pulse');
+            navBorderShow();
+        
+            if(opacityLevel==0) {
+                navbarAnimate.setAttribute("style",`opacity: ${opacityLevel}; visibility: hidden` );
+            } else {
+                navbarAnimate.setAttribute("style",`opacity: ${opacityLevel}; visibility: visible` );
+            }
+
+        }
+    }
+
+
+    //Fade-in Lateral do conteúdo
+    let $target = $('.anime-left, .anime-right'),
+    animationClass = 'anime-final'; 
+    let offset: number = <number>$(window).height()*(3/4);
+
+    function AnimateScroll(): any  {
+
+        let documentTop: number =<number>$(<Document>document).scrollTop();
+        $target.each(function(){
+            let itemTop = <number>$(this).offset()?.top;
+            
+            if(documentTop > (itemTop-offset)) {
+                $(this).addClass(animationClass);
+            } else {
+                $(this).removeClass(animationClass);
+            }
+        })       
+    }
+
+
+
+
+    $(document).scroll(debounce(function(){
+        AnimateFadeout();
+    },10));
+
+    $(document).scroll(throttle(function(){
+        AnimateScroll();
+    },200));
+
+    window.addEventListener('load', function(){
+        AnimateScroll();
+        AnimateFadeout();
+    })
+}());
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      minHeight: {
+        almostscreen: '86vh',
+        halfscreen: '50vh',
+      },
+      zIndex: {
+                 '-10': '-10', 
+      },
+      boxShadow: {
+        blue: '0 4px 14px 0 rgba(19, 51, 81, 0.39)',
+        white: '0 1px 3px 0 rgba(255,255,255, 0.5)',
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
